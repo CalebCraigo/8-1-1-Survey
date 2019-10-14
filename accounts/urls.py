c8ae91305a652aca98d2e79ef4d743c8a4fe8be3
CalebCraigo <CalebDOS1@yahoo.com>
2019-10-14 19:56:34 -0400

    add log in permissions for questions

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login'),
+    path('/login/', views.LoginView.as_view(), name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,6 @@ from django.urls import reverse_lazy
 
 class LoginView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('surveys:index')
+    success_url = reverse_lazy('surveys:create')
     template_name = 'login.html'
 # Create your views here.
